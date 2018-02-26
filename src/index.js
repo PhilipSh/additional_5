2b7c29f31f102c12da196f815796403d54990632
Philip Shpen <fil3120025@gmail.com>
2018-02-26 16:39:24 +0300

    50%%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var arr = str.split('');
+  var sum, maxSum;
+  sum = 0;
+  maxSum = -766766;
+  for (var k = 0; k < arr.length ; k++){
+  for(var i = 0; i < bracketsConfig.length; i++)
+  {
+  var sc = bracketsConfig[i];// достаем пару скобок
+
+ 
+    if(arr[k] == bracketsConfig[i][0]) {
+      ++sum;
+      if(sum >= maxSum){
+      maxSum = sum;
+      }
+     // else{
+      //  return false;
+     /// }
+    }
+    else{
+      --sum;
+      if(sum < 0 ) return false;
+    }
+  }
+}
+if(sum == 0){
+  return true;
+}
+else return false;
 }
